e2c099137954520c0534b1fa03da77b975e04584
Heather Olcot <hfolcot@gmail.com>
2018-08-23 20:21:25 +0000

    added functionality to create unique username

--- run.py
+++ run.py
@@ -1,13 +1,28 @@
 import os, json, random
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
+#Welcome page containing username entry, instructions and current high scores
 def index():
+    error = ""
+    # Handle POST request
+    if request.method == "POST":
+        usernames = open("data/users.txt").read()
+        if request.form["username"] in usernames:
+            error = "This username has already been taken"
+            return render_template("index.html", error=error)
+        else:
+            with open("data/users.txt", "a") as f:
+                f.writelines(request.form["username"] + "\n")
+                return redirect('/game')
     return render_template("index.html")
 
-app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
+@app.route("/game")
+#The page where the game will be played
+def game():
+    get_all_riddles("data/riddles.json")
 
 
 def get_all_riddles(riddle_file):
@@ -16,14 +31,5 @@ def get_all_riddles(riddle_file):
     """
     with open(riddle_file, "r") as f:
         all_riddles = json.load(f)
-
-def main():
-    get_all_riddles("data/riddles.json")
-    
-if __name__ == "__main__":
-    main()
-    """
-    Ensure the program only runs if specified by run.py
-    """
-
-    
+        
+app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
