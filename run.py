ef4948580dcad2a4483192b2b6fb7a58b624d9cb
Heather Olcot <hfolcot@gmail.com>
2018-10-24 09:14:49 +0000

    turned off debug

--- run.py
+++ run.py
@@ -180,4 +180,4 @@ def endgame(username):
 
 #Use the IF statement below to prevent the file from executing fully when imported by other modules
 if __name__ == '__main__':       
-    app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
+    app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=False)
